1a73e60e9429292adf93a83a143577db99874fcb
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2021-08-20 10:21:35 -0400

    Update sisEscola.js

--- JAVASCRIPT-BASICO/sisEscola.js
+++ JAVASCRIPT-BASICO/sisEscola.js
@@ -1,6 +1,6 @@
 var nomes=["Rodrigo","Igor"];
-var notasA=[7.3,8,2];
-var notasB=[3.4,1,3];
+var notasA=[4.3,8,2];
+var notasB=[7,9,3];
 
 function media(n1,n2){
     return(n1+n2)/2
@@ -18,5 +18,8 @@ for(var index in nomes){
     var nota1 = notasA[index]
     var nota2 = notasB[index]
 
-    console.log(nomes[index] + " - " + nota1 +" - " + nota2 + " - " + media(nota1, nota2) + " Resultado: " + passou())
+    var m = media(nota1, nota2)
+
+    console.log(nomes[index] + " - " + nota1 +" - " + nota2 + " - "
+    + media(nota1, nota2) + " Resultado: " + passou(m))
 }
