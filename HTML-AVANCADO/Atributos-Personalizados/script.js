3e0f191a809d6848d1666fe2de7d3d36f91d94f9
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2022-01-13 19:32:53 -0400

    Atributos personalizados

---
+++ HTML-AVANCADO/Atributos-Personalizados/script.js
@@ -0,0 +1,15 @@
+// let image = document.getElementById("like");
+
+// image.addEventListener("click", function () {
+//   image.setAttribute("src", "./images/dislike.jfif");
+// });
+
+let list = document.getElementById("lista");
+let num = parseInt(lista.getAttribute("num"));
+
+let conteudo = "";
+for (let i = 0; i < num; i++) {
+  conteudo += "<li>" + i + "</li>";
+}
+
+lista.innerHTML = conteudo;
